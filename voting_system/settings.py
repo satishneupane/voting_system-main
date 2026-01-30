689cc369beb1eaa3526536649c25eaddc8ff5ed0
rohanbaral16 <rhnbrl16@gmail.com>
2026-01-30 08:53:36 +0545

    allowed host changed

--- voting_system/settings.py
+++ voting_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-b+v9i@gm!(0jn-1=_0j04g&)v+knvmt%^*w0b$za&tglbeacd@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
 
 # Application definition
 
